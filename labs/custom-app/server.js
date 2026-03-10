466e0c563d48d7f1a305d68743839637207f0d00
PasiSandeepa <pasisandeepa456@gmail.com>
2026-03-10 12:26:52 +0530

    Completed all labs and added screenshots

---
+++ labs/custom-app/server.js
@@ -0,0 +1,13 @@
+const express = require('express'); 
+const app = express(); 
+const PORT = 3000; 
+app.get('/', (req, res) => { 
+  res.json({ 
+    message: 'Custom Docker App', 
+    version: '1.0.0', 
+    hostname: require('os').hostname() 
+  }); 
+}); 
+app.listen(PORT, '0.0.0.0', () => { 
+  console.log(`Server running on port ${PORT}`); 
+}); 
