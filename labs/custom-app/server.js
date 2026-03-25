92fdc17476c0fea232068839eb99b58a0d1809ec
KasuniMadhara <kasuniwijerathna2003@gmail.com>
2026-03-25 20:14:17 +0530

    Complete Docker 101 labs with screenshots

---
+++ labs/custom-app/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Custom Docker App',
+    version: '1.0.0'
+  });
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
